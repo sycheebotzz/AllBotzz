1f64cbd18636c7ae6c742a1dff4a2f659215ea5c
sycheebotzz <kevinkull188@gmail.com>
2025-01-15 20:23:48 +0800

    menu.js

---
+++ menu.js
@@ -0,0 +1,22 @@
+
+`Hallo @ silahkan memilih menu yg sudah disediakan`
+    
+> *Name* : 
+> *Owner* : @6282291427718⁩
+> *Mode* : Public
+> *Tanggal* : 
+> *Hari* : 
+> *Jam* : 00:00:00 
+  
+`simple menu AllBotzz`
+
+■ .allmenu
+■ .downloadmenu
+■ .ownermenu
+■ .groupmenu
+■ .funmenu
+■ .gamemenu
+■ .aimenu
+■ .toolsmenu
+
+> note : jika terjadi error, hubungi owner!!
