f573c5a6c8435ef91f4db59a625767e9260428e9
sycheebotzz <kevinkull188@gmail.com>
2025-01-15 20:46:09 +0800

    config.js

---
+++ config.js
@@ -0,0 +1,90 @@
+
+
+const chalk = require("chalk")
+const fs = require("fs")
+global.namastore = "ALL DOESN'T HAVE A SHOP"
+ global.namaowner = "erick"
+  global.ownername = "rick's"
+global.ownerNumber = ["-@s.whatsapp.net"] //6282194345945
+global.owner = ['-'] //6282194345945
+global.namaBot = "AllBotzz"
+global.namabot = "AllBotzz"
+global.nomerOwner = "-" //6282194345945
+global.nomorOwner = ['-'] //6282194345945
+global.botnum = ['-'] //6282291427718
+global.namaDeveloper = "TT:@syncheya" 
+global.packname = "rick'S"
+global.author = "Sticker By:AllBotzz"
+global.thumb = fs.readFileSync("./thumb.png")
+global.tqq = fs.readFileSync("./shoMedia/image/tqto.jpg")
+global.sessionName = 'session'
+global.tmn = fs.readFileSync("./shoMedia/image/tmenu.jpg")
+global.tekspushkon = ""
+global.tekspushkonv1 = ""
+global.tekspushkonv2 = ""
+global.tekspushkonv3 = ""
+global.saluran = "120363383347233294@newsletter"
+global.idsaluran = "120363383347233294@newsletter"
+global.gh = "https://github.com/sychyy"
+global.wagc = 'https://chat.whatsapp.com/GQ5Gp0eSeDS6dPBYeHE6kf'
+global.wach = "https://whatsapp.com/channel/0029Vb0v3F71yT264EejzJ3e"
+global.ibbKey = "e73afc3e94477852c77e41a41572fa8d"
+// Apikey
+global.APIs = {
+	alfa: 'https://api.zeeoneofc.my.id', //ganti sesukamu
+}
+global.APIKeys = {
+	'https://api.zeeoneofc.my.id': 'JKqddGlPzEcod4P', //ini juga
+}
+//━━━━━━━━━━━━━━━[ PAYMENT SETTING ]━━━━━━━━━━━━━━━━━//
+//
+global.thum = fs.readFileSync("./storage/image.png") 
+global.imqris = fs.readFileSync ("./storage/qris.png")
+//
+global.mess = {
+    admins: "lu bukan admin, kocak😂",
+    owners: "command itu cmn buat owner gua pntek",
+    dones: "udah selesai nih",
+    abots: "gua bkn admin",
+    groups: "ini kan di private chat, harusnya di grup"
+}
+global.pterodactylkey = {
+    domain: "",
+    apikey: "",
+    capikey: "",
+    eggs: "15",
+    location: "1"
+}
+global.limitawal = {
+    premium: "Infinity",
+    free: 20
+}
+global.APIs = {
+	hitori: 'https://my.hitori.pw/api',
+}
+global.APIKeys = {
+	'https://my.hitori.pw/api': 'htrkey-HysnOetF',
+}
+
+let file = require.resolve(__filename) 
+fs.watchFile(file, () => {
+fs.unwatchFile(file)
+console.log(chalk.redBright(`Update ${__filename}`))
+delete require.cache[file]
+require(file)
+})
+
+/*
+       •       #SCBOTWATERMUX #TERMUXNEVERDIE🔥   
+       •    #BOTWAONLYTERMUX #WHATSAPPBOTTERMUX
+       •       #SYCHYY #SYCHEE # SHOBOTZ #YURIN'S
+       
+   * 💬NOTE : 
+   * ⚠️ DILARANG MENJUAL SCRIPT INI WAHAI PARA JB KONTOL
+   * ⚠️ DILARANG MENGKLAIM BAHWA INI BOT BUATAN SENDIRI‼️
+   * ⚠️ JANGAN HAPUS KREDIT DIBAWAH‼️⚡
+   * 🔥 SUPPORT KAMI SHOBOTZ && SYCHYY && YURIN'S
+   * 👑 SC BY SHO - YuRin's [Yuda-Orlin]
+   * 💧 ©Sho-YuRin's - 2025
+   * 🔑 Nih Free htrkey-WnTtzEOtvhDj
+*/
